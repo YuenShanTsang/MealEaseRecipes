52233377db166b993097a319637cc341e241a7ad
yuenshan <A00270323@mycambrian.ca>
2023-07-10 17:53:21 -0400

    init commit

---
+++ MealEaseRecipes/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MealEaseRecipes;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MealEaseRecipes/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MealEaseRecipes;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
